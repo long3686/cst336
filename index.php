0f7346f343e3ce834974b91184cb24f139082901
Jared Long <jarlong@csumb.edu>
2017-09-03 05:56:31 +0000

    lab 1

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Jared Long: Personal Website </title>
+        <meta charset- "utf-8"/>
+    </head>
+    <body>
+        <header>
+              <h1> Jared Long </h1>
+        </header>
+      
+    </body>
+</html>
