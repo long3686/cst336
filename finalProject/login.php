d7797a8e60c12af2619063e83d4c9c892975ab53
Jared Long <jarlong@csumb.edu>
2017-12-08 11:27:18 +0000

    final commit thank christ

--- finalProject/login.php
+++ finalProject/login.php
@@ -1,33 +1,19 @@
 <?php
 session_start();
 
-if(isset ($_POST['username']))
-{
-    echo ($_POST['username']);
-    echo ($_POST['password']);
-}
-else
-{
-    echo "my code is dumb";
-}
 if (isset($_POST['loginForm'])) {  //login form has been submitted
     include '../dbConnection.php';
-    echo "no wait it is here";
     $dbConn = getDatabaseConnection();    
-    echo "here perhaps?";
      $sql = "SELECT * FROM admin " .
             " WHERE username = :username " .
              "AND password = :password  ";
     $stmt = $dbConn->prepare($sql);
-    echo "error here";
     $stmt->execute( array (":username" => $_POST['username'],
 		        ":password" => hash("sha1",$_POST['password'])));
-	echo "got here";
     $record = $stmt->fetch();
 
   if (!empty($record)) { //if record with username and password was found
         $_SESSION['username'] = $record['username'];
-        echo "yo i got here but still don't work?";
         $_SESSION['adminName'] = $record['lastName'];
         header("Location: adminPage.php");
     } else {
