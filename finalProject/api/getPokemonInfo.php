adaa31301f92484d1385060ea9f1f65c88692a22
Jared Long <jarlong@csumb.edu>
2017-12-08 10:21:59 +0000

    sql changes

--- finalProject/api/getPokemonInfo.php
+++ finalProject/api/getPokemonInfo.php
@@ -3,12 +3,12 @@
     include '../../../dbConnection.php';
     $dbConn = getDatabaseConnection(); 
     $sql = "SELECT * 
-    FROM pokemon p 
+    FROM Pokemon p 
     INNER JOIN type t on p.type1Id = t.typeId 
     WHERE p.type1Id = :typeId OR p.type2Id = :typeId";
     $stmt = $dbConn -> prepare($sql);
     $stmt -> execute(array("typeId"=>$_GET['typeId']));
-    $resultSet = $stmt->fetchAll();
+    $resultSet = $stmt->fetchAll(PDO::FETCH_ASSOC);
     echo json_encode($resultSet);
         
 ?>
